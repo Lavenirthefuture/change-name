6b4cb74b4a0e7c620cdee0029390d24c44344d02
Lavenirthefuture <964710108@qq.com>
2019-02-26 09:39:28 +0800

    后缀名

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 import os, sys
 
 # 打开文件
-path = "D:/聆听音乐"
+path = "J:/剧/瑞克和莫迪/瑞克 一般"
 dirs = os.listdir(path)
+letter = "S"
+
 
 # 输出所有文件和文件夹
 for file in dirs:
     print(file)
     # print(file.find("C"))
-    num = file.find("C")
-    list_name = "聆听音乐-" + file[num - 3:num - 1]+".rmvb"
+    num = file.find(letter)
+    list_name = "R&M " + file[num:num+6] + file[file.rfind("."):]
     print(list_name)
     os.rename(path + '/' + file, path + '/' + list_name)
 
